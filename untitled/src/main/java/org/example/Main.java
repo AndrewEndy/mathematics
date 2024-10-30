96ad5c8b030a6a433d498eba46be4a38b14e9484
cnnbless <antongolubchuk@gmail.com>
2024-10-30 16:23:33 +0200

    Додано виклик Multiplication у Main.java

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Multiplication multiplication = new Multiplication();
+        System.out.println("Результат множення: " + multiplication.multiply(5, 3));
     }
 }
